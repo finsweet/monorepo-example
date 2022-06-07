74b9e292ea190af470b02aec390c4a32dfc6ca52
Alex Iglesias <alexiglesias@finsweet.com>
2022-06-07 11:51:30 +0200

    Test commit

---
+++ .changeset/pretty-radios-double.md
@@ -0,0 +1,5 @@
+---
+"@finsweet/beta": patch
+---
+
+Removed console log
--- packages/beta/src/index.ts
+++ packages/beta/src/index.ts
@@ -2,4 +2,3 @@ import { TEST_VALUE } from '@finsweet/alpha';
 
 console.log(TEST_VALUE);
 console.log('hey!');
-console.log('ho!');
